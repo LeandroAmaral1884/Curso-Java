86c97e765e99ef4a1469dc4d0b6cf86b5fb84206
leandro.amaral <amaral4957@gmail.com>
2023-12-12 21:42:40 -0300

    If e Else e alterações de pasta

---
+++ EstruturaCondicao/EstruturaIfeElse/IfeElse/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+
+    
+    public static void main(String[] args) throws Exception {
+        int x = 5;
+        System.out.println("Bom dia");
+        if (x > 0) {
+            System.out.println("Boa tarde");
+        }
+
+        System.out.println("Boa noite");
+    }
+}
